d49a1776ef418e298fbaf5cfa48bfae96bbcbdc3
d.ramzin <d.ramzin@agimagroup.ru>
2020-12-19 23:01:40 +0300

    add debounce search to home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,18 +1,38 @@
-import React, { FC, ReactElement } from 'react';
+import React, { ChangeEvent, FC, ReactElement, useCallback } from 'react';
 
 import Search from 'antd/lib/input/Search';
 import Layout from 'antd/lib/layout/layout';
 import Title from 'antd/lib/typography/Title';
+import clsx from 'clsx';
+import debounce from 'lodash.debounce';
+import { useDispatch } from 'react-redux';
 
+import { fetchRepos } from '../../store/git-company/actions';
 import styles from './home.module.scss';
 
 const Home: FC = (): ReactElement => {
+  const dispatch = useDispatch();
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const fetchApi = useCallback(
+    debounce((value: string) => {
+      dispatch(fetchRepos(value));
+    }, 1000),
+    []
+  );
+
+  const handleChange = ({ target: { value } }: ChangeEvent<HTMLInputElement>): void => {
+    fetchApi(value);
+  };
+
+  const onSearch = (value: string) => fetchApi(value);
+
   return (
     <Layout className={styles.layout}>
-      <Title className={styles.title} level={3}>
+      <Title className={clsx(styles.title, styles.mainTitle)} level={2}>
         Welcome to Github-React app
       </Title>
-      <Title className={styles.title} level={5}>
+      <Title className={clsx(styles.title, styles.secondaryTitle)} level={4}>
         Search for the company
       </Title>
       <Search
@@ -20,7 +40,8 @@ const Home: FC = (): ReactElement => {
         placeholder="Find company on GitHub"
         enterButton="Search"
         size="large"
-        // loading
+        onChange={handleChange}
+        onSearch={onSearch}
       />
     </Layout>
   );
