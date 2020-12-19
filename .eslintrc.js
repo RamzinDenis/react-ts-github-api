68a44a317aafba4452e804ff3f8a2c9748028e9b
d.ramzin <d.ramzin@agimagroup.ru>
2020-12-19 17:56:18 +0300

    configure eslint, add react-roouter-dom

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,61 @@
 module.exports = {
   extends: [
-    "airbnb-typescript",
-    "airbnb/hooks",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:jest/recommended",
-    "prettier",
-    "prettier/react",
-    "prettier/@typescript-eslint",
-    "plugin:prettier/recommended",
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'prettier',
+    'prettier/react',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
   ],
-  plugins: ["react", "@typescript-eslint", "jest"],
+  plugins: ['react', '@typescript-eslint', 'jest'],
   env: {
     browser: true,
     es6: true,
     jest: true,
   },
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 2020,
-    sourceType: "module",
-    project: "./tsconfig.json",
+    sourceType: 'module',
+    project: './tsconfig.json',
     tsconfigRootDir: __dirname,
     createDefaultProgram: true,
   },
   rules: {
-    "linebreak-style": "off",
-    "prettier/prettier": [
-      "error",
+    'linebreak-style': 'off',
+    'import/prefer-default-export': 'off',
+    'prettier/prettier': [
+      'error',
       {
-        endOfLine: "auto",
+        endOfLine: 'auto',
+      },
+    ],
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal'],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'external',
+            position: 'before',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
       },
     ],
   },
